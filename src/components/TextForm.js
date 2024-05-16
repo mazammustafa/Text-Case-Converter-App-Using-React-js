c816cf307e348d0af1022b6e3ca69a366a96182f
mazammustafa <bscs20-089@ssuet.edu.pk>
2024-05-16 09:39:50 -0700

    in Progress

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,19 +2,57 @@ import React, { useState } from 'react';
 
 const TextForm = (props) => {
     const [text, setText] = useState('');
+    const [fontStyle, setfontStyle] = useState('normal');
+    const [convertCaseBtn, setconvertCaseBtn] = useState('Convert To UpperCase');
+    const [convertItalicBtn, setconvertItalicBtn] = useState('Convert To Italic');
+    // const [textDecoration, settextDecoration] = useState('normal');
 
-    // for uppercase
-    const handleUpperCase = () => {
-        setText(text.toUpperCase());
+    // for uppercase & lowercase
+    const handleTextCase = () => {
+        if (!text) {
+            alert("Enter Your Text")
+        } else {
+            if (text == text.toUpperCase()) {
+                setText(text.toLowerCase());
+                setconvertCaseBtn('Convert To UpperCase');
+    
+            }
+            else {
+                setText(text.toUpperCase());
+                setconvertCaseBtn('Convert To LowerCase');
+            }
+            let caserecord = document.getElementById("caserecord")
+            caserecord.innerHTML += `<li><button class="btn btn-primary">${convertCaseBtn}</button></li>` 
+        }
     }
 
-    // for lowercase
-    const handleLowerCase = () => {
-        setText(text.toLowerCase());
-    }
+    // // for uppercase
+    // const handleUpperCase = () => {
+    //     setText(text.toUpperCase());
+    // }
+    // // for lowercase
+    // const handleLowerCase = () => {
+    //     setText(text.toLowerCase());
+    // }
 
     const handleClearText = () => {
         setText("");
+    }
+    const handleItalicText = () => {
+        if (!text) {
+            alert("Enter your Text Here")
+        } else {
+            if (fontStyle == 'normal') {
+                setfontStyle('italic');
+                setconvertItalicBtn('Convert to Normal');
+            } else {
+                setfontStyle('normal');
+                setconvertItalicBtn('Convert to Italic');
+            }
+            
+                let caserecord = document.getElementById("caserecord")
+                caserecord.innerHTML += `<li><button class="btn btn-primary">${convertItalicBtn}</button</li>`
+        }
     }
 
     // for input change 
@@ -27,11 +65,18 @@ const TextForm = (props) => {
             <div className="container my-3">
                 <h1>{props.headings}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" id="myBox" rows="10" value={text} placeholder='Enter Your Text Here..' onChange={handleOnChange}></textarea>
+                    <textarea className="form-control" id="myBox" rows="10" value={text} placeholder='Enter Your Text Here..' onChange={handleOnChange} style={{ fontStyle: fontStyle }}></textarea>
                 </div>
-                <button className='btn btn-primary me-2' onClick={handleUpperCase}>Convert To UpperCase</button>
-                <button className='btn btn-primary me-2' onClick={handleLowerCase}>Convert To LowerCase</button>
+                <button className='btn btn-primary me-2' onClick={handleTextCase} id="caseBtn">{convertCaseBtn}</button>
+                {/* <button className='btn btn-primary me-2' onClick={handleLowerCase}>Convert To LowerCase</button> */}
                 <button className='btn btn-primary me-2' onClick={handleClearText}>Clear Text</button>
+                <button className='btn btn-primary me-2' onClick={handleItalicText}>{convertItalicBtn}</button>
+            </div>
+            
+
+            <div className="container border border-1">
+                <h1>Case Records</h1>
+                <ol id="caserecord" className='d-flex gap-5 flex-wrap'></ol>
             </div>
             <div className="container">
                 <h1 className='my-3'>Text Preview: </h1>
